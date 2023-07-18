0cc6ecae6c687e9e5b42d5cdd2ad19bd562a9b4c
byeongseokim <kbs970318@gmail.com>
2023-07-18 13:14:42 +0900

    Express로 sns 만들기

---
+++ nodebird/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const passport = require("passport");
+
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+const { join, login, logout } = require("../controllers/auth");
+
+const router = express.Router();
+
+// POST /auth/join
+router.post("/join", isNotLoggedIn, join);
+
+// POST /auth/login
+router.post("/login", isNotLoggedIn, login);
+
+// GET /auth/logout
+router.get("/logout", isLoggedIn, logout);
+
+module.exports = router;
---
+++ nodebird/routes/page.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares");
+const {
+  renderProfile,
+  renderJoin,
+  renderMain,
+} = require("../controllers/page");
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.use;
+  res.locals.followerCount = 0;
+  res.locals.followingCount = 0;
+  res.locals.followingIdList = [];
+  next();
+});
+
+router.get("/profile", isLoggedIn, renderProfile);
+
+router.get("/join", isNotLoggedIn, renderJoin);
+
+router.get("/", renderMain);
+
+module.exports = router;
